7e5bb58758d93b5b5386964a9eae108cc5da97ce
MrCakes931 <Mr.cakes931@gmail.com>
2026-03-19 23:03:09 -0600

    MrCakes931: [CURE] Phase 2a.1 - Stochastic Verifier

--- src/cures/adversarial/phase-2a-1-stochastic-verifier.js
+++ src/cures/adversarial/phase-2a-1-stochastic-verifier.js
@@ -2,7 +2,7 @@
 /**
  * CUI BONO CURE 2a.1: STOCHASTIC LOGIC VERIFIER
  * Neutralizes: Semantic Hallucination & Meta-Comment Injection.
- * Method: Recursive structural hashing and comment-stripping prior to logic ingestion.
+ * Method: Mandatory stringification and comment-regex-stripping prior to object reconstruction.
  */
 const LogicVerifier = {
     verify(obj) {
